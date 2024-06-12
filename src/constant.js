19aebef16ae8db6e71b7da32b0f1266685b405d9
aenulfahir <aenulfahir03@gmail.com>
2024-06-12 23:10:43 +0800

    menambahkan API Weather

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,2 @@
 export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
-export const API_KEY = 'API Key yang telah didapatkan';
+export const API_KEY = '4f0e1ae17f0773a42fee04b1e89db781';
